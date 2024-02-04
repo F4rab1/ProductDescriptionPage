ad63bc34f9a978ccb76fb48deb00aefb09f6968d
F4rab1 <issafarabi2022@gmail.com>
2024-02-04 15:00:45 +0600

    Tableview

--- ProductDescription/ViewController.swift
+++ ProductDescription/ViewController.swift
@@ -8,224 +8,153 @@
 import UIKit
 import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var screenWidth: CGFloat = 0
-    
-    let backButton: UIButton = {
-        let button = UIButton()
-        button.setImage(UIImage(named: "backButtonBlack"), for: .normal)
+
+    private let productImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "kurt")
         
-        return button
+        return imageView
     }()
-    
-    @objc func backButtonPressed() {
-        print("Back Button pressed")
-    }
-    
-    let productView: UIImageView = {
-        let iv = UIImageView()
-        iv.contentMode = .scaleAspectFit
-        iv.backgroundColor = UIColor(hexString: "#F5F5F5")
-        iv.image = UIImage(named: "kurt")
-        
-        return iv
-    }()
-    
-    let titleLabel: UILabel = {
+
+    private let nameLabel: UILabel = {
         let label = UILabel()
-        label.textColor = .black
-        label.textAlignment = .center
-        label.text = "Kurt Shoro light 30 g"
         label.font = UIFont.boldSystemFont(ofSize: 18)
-        
-        return label
-    }()
-    
-    let priceLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .systemGray3
-        label.textAlignment = .center
-        label.text = "405₸/pc"
-        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        
-        return label
-    }()
-    
-    let stackView: UIStackView = {
-        let sv = UIStackView()
-        sv.axis = .horizontal
-        sv.distribution = .fillEqually
-        sv.spacing = 10
-        
-        return sv
-    }()
-    
-    let description1: UILabel = {
-        let label = UILabel()
-        label.text = "sour"
-        label.textAlignment = .center
-        label.backgroundColor = UIColor(hexString: "#F5F5F5")
-        label.layer.cornerRadius = 10
-        label.clipsToBounds = true
-        
-        return label
-    }()
-    
-    let description2: UILabel = {
-        let label = UILabel()
-        label.text = "milk"
-        label.textAlignment = .center
-        label.backgroundColor = UIColor(hexString: "#F5F5F5")
-        label.layer.cornerRadius = 10
-        label.clipsToBounds = true
-        
-        return label
-    }()
-    
-    let description3: UILabel = {
-        let label = UILabel()
-        label.text = "desc"
-        label.textAlignment = .center
-        label.backgroundColor = UIColor(hexString: "#F5F5F5")
-        label.layer.cornerRadius = 10
-        label.clipsToBounds = true
-        
-        return label
-    }()
-    
-    let lineView: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor(hexString: "#F5F5F5")
-        
-        return view
-    }()
-    
-    let descriptionLabel: UILabel = {
-        let label = UILabel()
         label.numberOfLines = 0
-        let attributedTitle = NSMutableAttributedString(string: "Description\n \n", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 15), NSAttributedString.Key.foregroundColor: UIColor.black])
-        attributedTitle.append(NSAttributedString(string: """
-        Qurt(Құрт) is a traditional Kazakh dairy product made from fermented milk, usually from sheep, goat, or cow milk. It is a type of dried or dehydrated curd that is formed into small, round balls. The process of making qurt involves separating the whey from the milk, and then the remaining curd is formed into small pieces and air-dried.
-        """, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 15), NSAttributedString.Key.foregroundColor: UIColor.lightGray]))
-        label.attributedText = attributedTitle
-        label.textAlignment = .justified
+        label.text = "Kurt Shoro light 30 g"
+        
         return label
     }()
-    
-    let greenView: UIImageView = {
-        let iv = UIImageView()
-        iv.backgroundColor = UIColor(hexString: "#53C957")
-        
-        return iv
-    }()
-    
-    let price2Label: UILabel = {
+
+    private let priceLabel: UILabel = {
         let label = UILabel()
-        label.textColor = .white
-        label.textAlignment = .center
-        label.text = "405₸/pc"
-        label.font = UIFont.systemFont(ofSize: 20)
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.text = "405₸"
         
         return label
     }()
-    
-    let addButton: UIButton = {
+
+    private let brandLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.text = "Brand Name"
+        
+        return label
+    }()
+
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.text = """
+                Qurt(Құрт) is a traditional Kazakh dairy product made from fermented milk, usually from sheep, goat, or cow milk. It is a type of dried or dehydrated curd that is formed into small, round balls. The process of making qurt involves separating the whey from the milk, and then the remaining curd is formed into small pieces and air-dried.Qurt(Құрт) is a traditional Kazakh dairy product made from fermented milk, usually from sheep, goat, or cow milk. It is a type of dried or dehydrated curd that is formed into small, round balls. The process of making qurt involves separating the whey from the milk, and then the remaining curd is formed into small pieces and air-dried.Qurt(Құрт) is a traditional Kazakh dairy product made from fermented milk, usually from sheep, goat, or cow milk. It is a type of dried or dehydrated curd that is formed into small, round balls. The process of making qurt involves separating the whey from the milk, and then the remaining curd is formed into small pieces and air-dried.
+                """
+        label.textAlignment = .justified
+        
+        return label
+    }()
+
+    private let addToCartButton: UIButton = {
         let button = UIButton()
-        button.setImage(UIImage(systemName: "plus"), for: .normal)
-        button.tintColor = .white
+        button.setTitle("Add to Cart", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 8
         
         return button
     }()
     
-    @objc func addButtonPressed() {
-        print("Add to cart button pressed")
-    }
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        screenWidth = UIScreen.main.bounds.width
+        tableView.dataSource = self
+        tableView.delegate = self
         
-        setupViews()
+        setupUI()
         setupConstraints()
         addTargets()
     }
 
-    func setupViews() {
+    private func setupUI() {
         view.backgroundColor = .white
-        stackView.addArrangedSubview(description1)
-        stackView.addArrangedSubview(description2)
-        stackView.addArrangedSubview(description3)
-        [productView, backButton, titleLabel, priceLabel, stackView, lineView, descriptionLabel, greenView, price2Label, addButton].forEach {
-            view.addSubview($0)
+        screenWidth = UIScreen.main.bounds.width
+
+        tableView.tableFooterView = UIView()
+
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
     }
     
-    func setupConstraints() {
-        backButton.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            make.height.equalTo(20)
-            make.width.equalTo(34)
-            make.leading.equalToSuperview().offset(10)
-        }
-        
-        productView.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.leading.trailing.equalToSuperview()
+    private func addTargets() {
+        addToCartButton.addTarget(self, action: #selector(addToCartButtonTapped), for: .touchUpInside)
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.selectionStyle = .none
+
+        cell.contentView.addSubview(productImageView)
+        productImageView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
             make.width.height.equalTo(screenWidth)
         }
-        
-        titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(productView.snp.bottom).offset(10)
+
+        cell.contentView.addSubview(nameLabel)
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(productImageView.snp.bottom).offset(16)
             make.centerX.equalToSuperview()
         }
-        
+
+        cell.contentView.addSubview(priceLabel)
         priceLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(5)
+            make.top.equalTo(nameLabel.snp.bottom).offset(5)
             make.centerX.equalToSuperview()
         }
-        
-        stackView.snp.makeConstraints { make in
-            make.top.equalTo(priceLabel.snp.bottom).offset(10)
-            make.leading.trailing.equalToSuperview().inset(20)
-            make.height.equalTo(32)
+
+        cell.contentView.addSubview(brandLabel)
+        brandLabel.snp.makeConstraints { make in
+            make.top.equalTo(priceLabel.snp.bottom).offset(8)
+            make.leading.trailing.equalToSuperview().inset(16)
         }
-        
-        lineView.snp.makeConstraints { make in
-            make.top.equalTo(stackView.snp.bottom).offset(10)
-            make.leading.trailing.equalToSuperview().inset(20)
-            make.height.equalTo(2)
-        }
-        
+
+        cell.contentView.addSubview(descriptionLabel)
         descriptionLabel.snp.makeConstraints { make in
-            make.top.equalTo(lineView).offset(20)
-            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(brandLabel.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
         }
-        
-        greenView.snp.makeConstraints { make in
-            make.bottom.equalToSuperview()
-            make.leading.trailing.equalToSuperview()
-            make.height.equalTo(100)
+
+        cell.contentView.addSubview(addToCartButton)
+        addToCartButton.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(16)
+            make.height.equalTo(44)
         }
-        
-        price2Label.snp.makeConstraints { make in
-            make.top.equalTo(greenView.snp.top).offset(10)
-            make.centerX.equalToSuperview()
-        }
-        
-        addButton.snp.makeConstraints { make in
-            make.leading.equalTo(price2Label.snp.trailing).offset(10)
-            make.top.equalTo(greenView.snp.top).offset(6)
-            make.width.height.equalTo(35)
-        }
+
+        return cell
     }
-    
-    func addTargets() {
-        backButton.addTarget(self, action: #selector(backButtonPressed), for: .touchUpInside)
-        addButton.addTarget(self, action: #selector(addButtonPressed), for: .touchUpInside)
+
+    @objc private func addToCartButtonTapped() {
+        print("add to cart button tapped!")
     }
-    
 }
 
 extension UIColor {
@@ -247,4 +176,7 @@ extension UIColor {
         self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
     }
 }
+
+
+
 
